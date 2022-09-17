be0582b9e840b67fb9f1a3a3abea19821dba9f26
stanislawsztrajt <stanislawsztrajt@wp.pl>
2022-09-17 21:24:23 +0200

    Changed backend to production, changed api url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+//
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3001', 'https://forum-nestjs-next-ts.vercel.app'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
