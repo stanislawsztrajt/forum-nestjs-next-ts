2824e2a54372dafb9463ed8f818020199ea94df5
stanislawsztrajt <stanislawsztrajt@wp.pl>
2022-09-17 20:24:47 +0200

    Removed export types from topics

--- frontend/src/features/topics/index.ts
+++ frontend/src/features/topics/index.ts
@@ -2,6 +2,5 @@ import CreateTopicForm from './create-topic-form';
 import TopicItem from './topic-item';
 import TopicList from './topic-list';
 import TopicsSearchInput from './topics-search-input';
-import Types from './types';
 
-export { CreateTopicForm, TopicItem, TopicList, TopicsSearchInput, Types };
+export { CreateTopicForm, TopicItem, TopicList, TopicsSearchInput };
